8c3379787072310f8936b41f0fc6201dde45d4e7
Alex <variuhin@yandex.ru>
2021-05-13 17:27:50 +0300

    Create gallery image component

---
+++ src/components/GalleryImage.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <v-container class="gallery-image">
+    <v-card max-width="250" max-height="350">
+      <v-img :src="url" max-width="250" max-height="250"></v-img>
+      <v-card-title>
+        {{title}}
+      </v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'GalleryImage',
+  props: {
+    url: String, title: String,
+  }
+}
+</script>
