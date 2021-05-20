abd590d91b1de28a5eb1d08c0c91206cb7d806a1
Alex <variuhin75@gmail.com>
2021-05-20 14:28:57 +0300

    Add categories

---
+++ src/components/Category.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="category">
+    <v-row>
+      <v-col v-for="image in images" :key="image.id">
+        <GalleryImage
+          :url="image.url"
+          :title="image.title"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import GalleryImage from '@/components/GalleryImage.vue'
+  export default {
+    props: {
+       images: GalleryImage
+    },
+    components: {
+      GalleryImage
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,44 @@
 <template>
   <v-container class="home">
-    <v-row>
-      <v-col v-for="image in images" :key="image.id">
-        <GalleryImage
-          :url="image.url"
-          :title="image.title"
-        />
-      </v-col>
-    </v-row>
+    <v-tabs centered>
+      <v-tab v-for="n in categoriesNum" :key="n">category {{n}}</v-tab>
+      <v-tab-item v-for="n in categoriesNum" :key="n">
+        <Category :images="getCategoryImages(n)"/>
+      </v-tab-item>
+    </v-tabs>
   </v-container>
 </template>
 
 <script>
-  import {mapState, mapActions} from 'vuex'
-  import GalleryImage from '@/components/GalleryImage.vue'
-  import {getData} from '@/utils.js'
   const url = 'https://jsonplaceholder.typicode.com/photos/'
+
+  import {mapState, mapActions} from 'vuex'
+  import Category from '@/components/Category.vue'
+  import {getData} from '@/utils.js'
+
   export default {
+    data(){ return {categoriesNum: 4, iamgesNum: 24} },
     mounted(){
       getData(
         url,
         (data) => {
-          const slicedData = data.slice(0, 24)
+          const slicedData = data.slice(0, this.iamgesNum)
           this.setImagesDelayed(slicedData)
         },
         (err) => console.error(err)
       )
     },
     computed: mapState(['images']),
-    methods: mapActions(['setImagesDelayed']),
+    methods: {
+      getCategoryImages(num){
+        const images = [...this.images]
+        const imagesInCategoryNum = this.iamgesNum/this.categoriesNum
+        return images.splice((num-1)*imagesInCategoryNum, imagesInCategoryNum)
+      },
+      ...mapActions(['setImagesDelayed'])
+    },
     components: {
-      GalleryImage
+      Category
     }
   }
 </script>
