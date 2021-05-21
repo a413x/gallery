69da437de065fb540bd4d908fefa7c580af1fe09
Alex <variuhin75@gmail.com>
2021-05-21 14:18:08 +0300

    Add image details

---
+++ src/components/Details.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container class="details" v-if="imageInfo">
+    <v-img :src="imageInfo.url" max-width="600" max-height="600"></v-img>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    props: ['id'],
+    computed: {
+      imageInfo(){ return this.$store.getters.getImageById(+this.id) }
+    }
+  }
+</script>
