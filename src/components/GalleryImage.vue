6f9bff64d3ba8c21f9ba945bebdc5b52c0438f70
Alex <variuhin75@gmail.com>
2021-05-21 14:50:21 +0300

    Add details button to image card

--- src/components/GalleryImage.vue
+++ src/components/GalleryImage.vue
@@ -1,11 +1,20 @@
 <template lang="html">
   <v-container class="gallery-image">
-    <v-card max-width="250" max-height="350">
-      <v-img :src="imageInfo.url" max-width="250" max-height="250"></v-img>
-      <v-card-title>
-        {{cuttedTitle}}
-      </v-card-title>
-    </v-card>
+    <v-hover>
+      <template v-slot:default="{ hover }">
+        <v-card max-width="250" max-height="350">
+          <v-img :src="imageInfo.url" max-width="250" max-height="250"></v-img>
+          <v-card-title>
+            {{cuttedTitle}}
+          </v-card-title>
+          <v-fade-transition>
+            <v-overlay v-if="hover" absolute>
+              <v-btn @click="onDetailsClick">Details</v-btn>
+            </v-overlay>
+          </v-fade-transition>
+        </v-card>
+      </template>
+    </v-hover>
   </v-container>
 </template>
 
@@ -15,8 +24,11 @@ export default {
   name: 'GalleryImage',
   props: { imageInfo: Object },
   computed: {
-    cuttedTitle(){
-      return cutTitle(this.imageInfo.title, 40)
+    cuttedTitle(){ return cutTitle(this.imageInfo.title, 40) }
+  },
+  methods: {
+    onDetailsClick(){
+      this.$router.push({ path: `/details/${this.imageInfo.id}` })
     }
   }
 }
