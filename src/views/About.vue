e0f85ff1479ca475b157caa206c449905303579c
Alex <variuhin75@gmail.com>
2021-05-27 13:38:14 +0300

    Fill the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    about
-  </div>
+  <v-container class="about">
+    <div>Alexander</div>
+    <div>variuhin75@gmail.com</div>
+
+    <v-btn icon href="https://github.com/a413x">
+      <v-icon large>mdi-github</v-icon>
+    </v-btn>
+    <v-btn icon href="https://codepen.io/a413x">
+      <v-icon large>mdi-codepen</v-icon>
+    </v-btn>
+  </v-container>
 </template>
